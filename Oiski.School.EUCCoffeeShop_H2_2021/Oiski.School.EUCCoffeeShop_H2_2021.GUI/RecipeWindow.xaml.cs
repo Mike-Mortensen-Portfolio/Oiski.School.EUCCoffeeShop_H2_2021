3bb19bb616856c018397e819b351766838d6c26d
Mike Mortensen <Zhakalen@Gmail.com>
2021-07-21 09:14:48 +0200

    Hotfix 1 ~ Fixed InitializeComponent method and various components fields not being reconized by the compiler ~ Fixed and issue where the program would crash when the coffee list is empty on start up

--- Oiski.School.EUCCoffeeShop_H2_2021/Oiski.School.EUCCoffeeShop_H2_2021.GUI/RecipeWindow.xaml.cs
+++ Oiski.School.EUCCoffeeShop_H2_2021/Oiski.School.EUCCoffeeShop_H2_2021.GUI/RecipeWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using Oiski.School.EUCCoffeeShop_H2_2021.Utility;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Oiski.School.EUCCoffeeShop_H2_2021.GUI
 {
@@ -22,38 +12,42 @@ namespace Oiski.School.EUCCoffeeShop_H2_2021.GUI
     /// </summary>
     public partial class RecipeWindow : Window
     {
-        private ObservableCollection<string> coffees;
+        private readonly ObservableCollection<string> coffees;
 
-        public RecipeWindow ()
+        public RecipeWindow()
         {
-            InitializeComponent ();
-            coffees = new ObservableCollection<string> ();
+            InitializeComponent();
+            coffees = new ObservableCollection<string>();
 
-            foreach ( CoffeeShop.Coffee coffee in CoffeeRepo.Link.GetEnumerable () )
+            foreach (CoffeeShop.Coffee coffee in CoffeeRepo.Link.GetEnumerable())
             {
-                coffees.Add (coffee.Name);
+                coffees.Add(coffee.Name);
             }
 
             coffeeList.ItemsSource = coffees;
-            coffeeList.SelectedItem = coffeeList.Items[ 0 ];
+
+            if (coffeeList.Items.Count > 0)
+            {
+                coffeeList.SelectedItem = coffeeList.Items[0];
+            }
         }
 
-        private void MissingLogic ( object _sender, RoutedEventArgs _r )
+        private void MissingLogic(object _sender, RoutedEventArgs _r)
         {
-            if ( _r.Source is Button btn && btn.Command == null && btn.CommandParameter == null )
+            if (_r.Source is Button btn && btn.Command == null && btn.CommandParameter == null)
             {
                 btn.IsEnabled = false;
-                MessageBox.Show ("No Commands Attached");
+                MessageBox.Show("No Commands Attached");
             }
 
             _r.Handled = true;
         }
 
-        private void coffeeList_SelectionChanged ( object sender, SelectionChangedEventArgs e )
+        private void CoffeeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if ( sender is ComboBox cmbx )
+            if (sender is ComboBox cmbx)
             {
-                CoffeeShop.Coffee cof = CoffeeRepo.Link.GetEnumerable ().ToList ().Find (item => item.Name == cmbx.SelectedItem.ToString ());
+                CoffeeShop.Coffee cof = CoffeeRepo.Link.GetEnumerable().ToList().Find(item => item.Name == cmbx.SelectedItem.ToString());
                 coffeeDisplay.Text = $"Name: {cof.Name}{Environment.NewLine}Description: {cof.Description}{Environment.NewLine}Ingredients:{Environment.NewLine}{cof.GetIngredientsAsString}";
             }
         }
